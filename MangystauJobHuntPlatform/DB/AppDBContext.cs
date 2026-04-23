4d72007b1a6fc67655989c64f5ba0ed85f831a60
Alimurat <alimratnurgajn@gmail.com>
2026-04-24 01:15:24 +0500

    Adding base db interaction for future

---
+++ MangystauJobHuntPlatform/DB/AppDBContext.cs
@@ -0,0 +1,18 @@
+using MangystauJobHuntPlatform.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MangystauJobHuntPlatform.DB;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Users> Users { get; set; }
+    public DbSet<Vacancy> Vacancies { get; set; }
+    public DbSet<Application> Applications { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Users>().HasIndex(u => u.TelegramId).IsUnique();
+    }
+}
