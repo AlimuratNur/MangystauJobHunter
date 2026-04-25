cf12afcfb3ed1f4cb612dfe526a443640040dd0a
Alimurat <alimratnurgajn@gmail.com>
2026-04-25 06:14:57 +0500

    users upgrade

--- MangystauJobHuntPlatform/Models/Users.cs
+++ MangystauJobHuntPlatform/Models/Users.cs
@@ -8,4 +8,10 @@ public class Users
         public string Name { get; set; }
         public UserRole Role { get; set; }
         public string Skills { get; set; } // Сюда пишем навыки через запятую для AI
+        
+        public int Age { get; set; }
+        public string District { get; set; } // Например, "14"
+        public RegistrationStep Step { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
     }
