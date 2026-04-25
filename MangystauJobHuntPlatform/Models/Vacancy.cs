72acf4e75b43eb6413a150dc5e21edad26a1a08f
Alimurat <alimratnurgajn@gmail.com>
2026-04-25 09:05:31 +0500

    vacancy variable create

--- MangystauJobHuntPlatform/Models/Vacancy.cs
+++ MangystauJobHuntPlatform/Models/Vacancy.cs
@@ -6,7 +6,7 @@ public class Vacancy
     public string Title { get; set; }
     public string Description { get; set; }
     public string District { get; set; } // Например, "14 мкр" 
-    public string RequiredSkills { get; set; }
+    public string? RequiredSkills { get; set; }
     public long OwnerId { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public double Latitude { get; set; }
