049d81c8b389263fe3e98fc24defdfa4c241c51d
Alimurat <alimratnurgajn@gmail.com>
2026-04-25 06:15:18 +0500

    vacancy upgrade

--- MangystauJobHuntPlatform/Models/Vacancy.cs
+++ MangystauJobHuntPlatform/Models/Vacancy.cs
@@ -7,6 +7,8 @@ public class Vacancy
     public string Description { get; set; }
     public string District { get; set; } // Например, "14 мкр" 
     public string RequiredSkills { get; set; }
-    public int OwnerId { get; set; }
+    public long OwnerId { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
 }
