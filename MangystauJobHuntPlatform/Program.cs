6be132557e7b2f9fc0a54a41e4bb30bb05378214
Alimurat <alimratnurgajn@gmail.com>
2026-04-25 06:13:05 +0500

    adding more dependecies

--- MangystauJobHuntPlatform/Program.cs
+++ MangystauJobHuntPlatform/Program.cs
@@ -1,10 +1,46 @@
+using MangystauJobHuntPlatform.DB;
+using MangystauJobHuntPlatform.Interface;
+using MangystauJobHuntPlatform.Models;
+using MangystauJobHuntPlatform.Services;
+using MangystauJobHuntPlatform.Strategies;
+using Microsoft.EntityFrameworkCore;
+using Telegram.Bot;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+// Если захочешь сменить AI — просто замени CosineMatchingStrategy на другую реализацию
+builder.Services.AddScoped<IMatchingStrategy, CosineMatchingStrategy>();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite("Data Source=mangystau_jobs.db"));
+
+builder.Services.AddControllers();
+
+builder.Services.AddHttpClient("tgwebhook")
+    .AddTypedClient<ITelegramBotClient>(httpClient => 
+        new TelegramBotClient("8296938940:AAFa3S59LR7zVmQK7HNyXRGdkbF8Fb2Vjos", httpClient));
+builder.Services.AddHttpClient("tgwebhook")
+    .AddTypedClient<ITelegramBotClient>(httpClient => 
+        new TelegramBotClient("8262319697:AAGWpnSAB7kbxvXlNxRwNpCLBl123wRHYTQ", httpClient));
+
+// Регистрируем наш движок бота
+builder.Services.AddScoped<TelegramBotService>();
+
+// Фоновая служба, которая слушает сообщения
+builder.Services.AddHostedService<BotBackgroundService>();
+
+// Сначала HTTP клиент (нужен для AI)
+builder.Services.AddHttpClient<AiGeocodingService>();
+
+// AI сервис
+builder.Services.AddScoped<AiGeocodingService>();
+
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -14,28 +50,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
+
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
